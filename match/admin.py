1984f864ea66d0343a33fd9ecd8efb0d9dbdf26e
NoMariusz <mariusz2727@vp.pl>
2021-04-03 12:36:33 +0200

    Corrected managing MatchInformation in admin page
    Enabled to set blank winner field to MatchInformation in admin page

--- match/admin.py
+++ match/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 from .models import MatchInformation
 
 
-admin.site.register(MatchInformation)
+class MatchAdmin(admin.ModelAdmin):
+    def get_form(self, request, obj=None, **kwargs):
+        """ Override to enable set none winner in admin
+        site """
+        form = super(MatchAdmin, self).get_form(request, obj, **kwargs)
+        form.base_fields['winner'].required = False
+        return form
+
+
+admin.site.register(MatchInformation, MatchAdmin)
