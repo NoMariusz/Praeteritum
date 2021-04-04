0109bea01dcf0d1d30ee6d2f4a4c5644afac292f
NoMariusz <mariusz2727@vp.pl>
2021-04-04 11:14:56 +0200

    Improved information about running matches
    Enabled stacking snackbars with information and improved them responsivity

--- frontend/src/components/home/matches_notifer/RunningMatchInfo.js
+++ frontend/src/components/home/matches_notifer/RunningMatchInfo.js
@@ -1,37 +1,55 @@
 import React from "react";
 import { Button, Paper, Typography, Box } from "@material-ui/core";
+import {
+    createMuiTheme,
+    responsiveFontSizes,
+    ThemeProvider,
+} from "@material-ui/core/styles";
+
+let theme = createMuiTheme();
+theme = responsiveFontSizes(theme);
 
 export const RunningMatchInfo = ({ matchInfo, goToMatchCallback }) => {
     return (
-        <Paper>
-            <Box
-                p={1}
-                display="flex"
-                justifyContent="flex-end"
-                flexDirection="column"
-            >
-                <Typography variant="subtitle1" align="center">
-                    Running match...
-                </Typography>
-                <Typography variant="h4" color="primary" align="center">
-                    {matchInfo.players[0].username} vs{" "}
-                    {matchInfo.players[1].username}
-                </Typography>
-                <Box mt={1} display="flex" justifyContent="flex-end">
-                    <Button
-                        onClick={() => {
-                            goToMatchCallback(matchInfo.id);
-                        }}
-                        variant="outlined"
-                        color="secondary"
-                        align="right"
-                        size="small"
+        <ThemeProvider theme={theme}>
+            <Box width={{ xs: "10rem", sm: "20rem", md: "25rem" }}>
+                <Paper elevation={3}>
+                    <Box
+                        width={1}
+                        p={1}
+                        display="flex"
+                        justifyContent="flex-end"
+                        flexDirection="column"
                     >
-                        Reconnect
-                    </Button>
-                </Box>
+                        <Typography variant="subtitle1" align="center">
+                            Running match...
+                        </Typography>
+                        <Typography
+                            variant="h4"
+                            color="primary"
+                            align="center"
+                            noWrap="false"
+                        >
+                            {matchInfo.players[0].username} vs{" "}
+                            {matchInfo.players[1].username}
+                        </Typography>
+                        <Box mt={1} display="flex" justifyContent="flex-end">
+                            <Button
+                                onClick={() => {
+                                    goToMatchCallback(matchInfo.id);
+                                }}
+                                variant="outlined"
+                                color="secondary"
+                                align="right"
+                                size="small"
+                            >
+                                Reconnect
+                            </Button>
+                        </Box>
+                    </Box>
+                </Paper>
             </Box>
-        </Paper>
+        </ThemeProvider>
     );
 };
 
--- frontend/src/components/home/matches_notifer/RunningMatchesNotifer.js
+++ frontend/src/components/home/matches_notifer/RunningMatchesNotifer.js
@@ -1,63 +1,19 @@
-import React, { useState, useEffect } from "react";
-import { Snackbar } from "@material-ui/core";
-import { getCSRF, sleep } from "components/../utils";
-import RunningMatchInfo from "./RunningMatchInfo";
+import React from "react";
+import MatchesSnackbarsRenderer from "./MatchesSnackbarsRenderer";
+import { SnackbarProvider } from "notistack";
 
 export const RunningMatchesNotifer = ({ goToMatchCallback }) => {
-    /* displaying information about running matches */
+    /* display information about running matches */
 
-    const [runningMatches, setRunningMatches] = useState([]);
-
-    // searching for running matches to player
-
-    let live = true;
-
-    const startSearching = async () => {
-        while (live) {
-            searchForMatches();
-            await sleep(5000);
-        }
-    };
-
-    const searchForMatches = () => {
-        const requestOptions = {
-            headers: {
-                "Content-Type": "application/json",
-                "X-CSRFToken": getCSRF(),
-            },
-        };
-        fetch("/match-api/active-matches", requestOptions).then((res) => {
-            if (res.ok) {
-                res.json().then((data) => {
-                    setRunningMatches(data.active_matches);
-                });
-            } else {
-                console.error("Can not get runningMAtches information");
-            }
-        });
-    };
-
-    // start loop searching for running matches
-
-    useEffect(() => {
-        startSearching();
-    }, []);
-
-    // rendering
-
-    return runningMatches.map((runningMatch) => (
-        <Snackbar
+    return (
+        <SnackbarProvider
+            maxSnack="3"
             anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
-            open={true}
-            onClose={null}
-            message="You have running Match"
+            autoHideDuration={null}
         >
-            <RunningMatchInfo
-                matchInfo={runningMatch}
-                goToMatchCallback={goToMatchCallback}
-            />
-        </Snackbar>
-    ));
+            <MatchesSnackbarsRenderer goToMatchCallback={goToMatchCallback} />
+        </SnackbarProvider>
+    );
 };
 
 export default RunningMatchesNotifer;
