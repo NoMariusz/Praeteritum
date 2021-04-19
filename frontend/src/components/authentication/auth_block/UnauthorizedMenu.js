f32e00712631a87860ab8529e650a3ab329193c2
NoMariusz <mariusz2727@vp.pl>
2021-04-19 16:23:15 +0200

    Improved UnauthorizedMenu look and responsiveness

--- frontend/src/components/authentication/auth_block/UnauthorizedMenu.js
+++ frontend/src/components/authentication/auth_block/UnauthorizedMenu.js
@@ -1,19 +1,87 @@
-import React from "react";
-import { Button, ButtonGroup } from "@material-ui/core";
+import React, { useState } from "react";
+import {
+    Box,
+    Button,
+    ButtonGroup,
+    Link,
+    IconButton,
+    Menu,
+    MenuItem,
+} from "@material-ui/core";
+import { MoreVert } from "@material-ui/icons";
 
 export const UnauthorizedMenu = () => {
     /* mini menu displaying when user is not authenticated, enable to log in
     and register */
 
+    // menu stuff
+
+    const [anchorEl, setAnchorEl] = useState(null);
+
+    const openMenu = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const closeMenu = () => {
+        setAnchorEl(null);
+    };
+
     return (
-        <ButtonGroup
-            variant="text"
-            color="secondary"
-            aria-label="auth button group"
-        >
-            <Button href="/login">Login</Button>
-            <Button href="/register">Register</Button>
-        </ButtonGroup>
+        <>
+            <Box display={{ xs: "none", sm: "block" }}>
+                <ButtonGroup
+                    variant="text"
+                    color="inherit"
+                    aria-label="auth button group"
+                >
+                    <Button href="/login">Login</Button>
+                    <Button href="/register">Register</Button>
+                </ButtonGroup>
+            </Box>
+            <Box display={{ xs: "block", sm: "none" }}>
+                <>
+                    <IconButton
+                        aria-label="Auth menu"
+                        color="inherit"
+                        edge="end"
+                        onClick={openMenu}
+                    >
+                        <MoreVert />
+                    </IconButton>
+                    <Menu
+                        id="auth-menu"
+                        anchorEl={anchorEl}
+                        keepMounted
+                        open={Boolean(anchorEl)}
+                        onClose={closeMenu}
+                        getContentAnchorEl={null}
+                        anchorOrigin={{
+                            vertical: "bottom",
+                            horizontal: "center",
+                        }}
+                        transformOrigin={{
+                            vertical: "top",
+                            horizontal: "center",
+                        }}
+                    >
+                        <MenuItem
+                            component={Link}
+                            href="/login"
+                            underline="none"
+                        >
+                            Login
+                        </MenuItem>
+                        <MenuItem
+                            component={Link}
+                            href="/login"
+                            underline="none"
+                        >
+                            Register
+                        </MenuItem>
+                    </Menu>
+                </>
+            </Box>
+        </>
     );
 };
 
