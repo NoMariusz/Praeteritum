96009dc6ab963c2aad6231438712a49a1d0e9ece
NoMariusz <mariusz2727@vp.pl>
2021-05-03 11:24:36 +0200

    Simplified date in main section in game menu

--- playerdata/serializers.py
+++ playerdata/serializers.py
@@ -3,6 +3,9 @@ from django.contrib.auth.models import User
 
 
 class UserInformationShort(serializers.ModelSerializer):
+    date_joined = serializers.DateTimeField(
+        format="%Y-%m-%d", required=False, read_only=True)
+
     class Meta:
         model = User
         fields = ("username", "email", "date_joined")
