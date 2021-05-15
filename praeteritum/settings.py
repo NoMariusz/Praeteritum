528b31ffb4955baa04ecf07f65f752e11af8211c
NoMariusz <mariusz2727@vp.pl>
2021-05-15 12:52:46 +0200

    Hid Django SECRET_KEY in .env

--- praeteritum/settings.py
+++ praeteritum/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'b1)+n#j)5)r7@9-bm73hvr7&3ll!+r!pfqz+!#-2!^ajckii4#'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
