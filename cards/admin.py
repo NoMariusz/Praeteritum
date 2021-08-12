6f2d8888833abfa2ef0f2fb890f1aa1fba75cfaa
NoMariusz <mariusz2727@vp.pl>
2021-08-12 18:31:51 +0200

    Fixed issue with Card form at admin page

--- cards/admin.py
+++ cards/admin.py
@@ -8,7 +8,6 @@ class CardAdmin(admin.ModelAdmin):
         site '''
         form = super(CardAdmin, self).get_form(request, obj, **kwargs)
         form.base_fields['image'].required = False
-        form.base_fields['effect'].required = False
         return form
 
 
