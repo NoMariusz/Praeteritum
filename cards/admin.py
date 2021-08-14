427f762f078c6041da160133444afe75005d4d01
NoMariusz <mariusz2727@vp.pl>
2021-08-14 15:31:50 +0200

    Extended admin page Card section
    Made easier to manage cards

--- cards/admin.py
+++ cards/admin.py
@@ -3,6 +3,12 @@ from .models import CardModel
 
 
 class CardAdmin(admin.ModelAdmin):
+    list_display = (
+        'name', 'kind', 'category', 'rarity', 'attack', 'hp', 'card_strength',
+        'defaultInCollection', 'defaultInDeck')
+    list_filter = (
+        'kind', 'category', 'rarity', 'defaultInCollection', 'defaultInDeck')
+
     def get_form(self, request, obj=None, **kwargs):
         ''' Override to enable set blank image and effect card fields in admin
         site '''
